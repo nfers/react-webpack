08ff7365eab065d26647db584c3b8cd6bf0552cb
Nayara - Home <ncy.fers@gmail.com>
2019-09-27 18:35:45 -0300

    2019-09-27-#10: criando componentes de button e square

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './components/Title/title'
+import Button from './components/Button/buttonIndex'
+import Square from './components/Square/squareIndex'
 
-class App extends React.Component {
+class App extends Component {
   render () {
     this.props
-    return (
-      <div className='container'>
-      <Title name='Nayara' lastname='Ferreira' /> 
-      
+    return (     
+      <div className='container' onClick={(e) => {
+        alert('ok')
+      }}>
+      <div> 
+        {['blue', 'red', 'gray'].map((square) => (
+         <Square key={square} color={square} /> 
+        ))}
+      </div>
+      <Title name='Nayara' lastname='Ferreira' />     
+      <div className='container' id='1'>
+        <Button> 
+          <span> tex</span>  
+        </Button>
+      </div>
     </div> 
     )
   }
