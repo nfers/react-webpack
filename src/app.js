c00f4d1d6e107308f3684a8dc4ab9bd3c3bb93da
Nayara - Home <ncy.fers@gmail.com>
2019-09-29 21:08:59 -0300

    2019-09-29-#12: criando formulario no componente app

--- src/app.js
+++ src/app.js
@@ -1,29 +1,49 @@
 'use strict'
 
 import React, { Component } from 'react'
+//import Timer from './components/Timer/index'
 import Title from './components/Title/title'
 import Button from './components/Button/buttonIndex'
 import Square from './components/Square/squareIndex'
 
 class App extends Component {
+   constructor () {
+  //   console.log('constructor')
+     super()
+     this.state = {
+      value: 'Inicio', 
+      checked: false
+     }
+   }
   render () {
-    this.props
     return (     
-      <div className='container' onClick={(e) => {
-        alert('ok')
-      }}>
-      <div> 
-        {['blue', 'red', 'gray'].map((square) => (
-         <Square key={square} color={square} /> 
-        ))}
+      <div>
+        <form>
+          <input type='text' value={this.state.value} 
+            onChange={(e) => {
+              this.setState({
+                value: e.target.value
+            })
+          }}/>
+          <label>
+            <input type='checkbox' 
+              value='my-checkbox' 
+              checked={this.state.checked}
+              onChange={(e) => {
+                this.setState({ checked: !this.state.checked })}}/>
+            Check
+          </label>
+        </form>
       </div>
-      <Title name='Nayara' lastname='Ferreira' />     
-      <div className='container' id='1'>
-        <Button> 
-          <span> tex</span>  
-        </Button>
-      </div>
-    </div> 
+/*
+        { <Square color={this.state.color}/>
+
+        {['red', 'purple', 'green'].map((color) => (
+          <Button 
+          key={color} 
+          handleClick={() => this.setState({ color })}>
+            {color} </Button>
+        ))} } */
     )
   }
 }
