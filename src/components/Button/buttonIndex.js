e50c74d4d4c70388156c8cd7f6c1d7244cd54b98
Nayara - Home <ncy.fers@gmail.com>
2019-09-29 21:09:40 -0300

    2019-09-29-#13: aninhando componentes button

--- src/components/Button/buttonIndex.js
+++ src/components/Button/buttonIndex.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const Button = ({ children }) => (
-    <button>{children}</button>
+const Button = ({ children, handleClick }) => (
+    <button onClick={handleClick}>
+        {children}
+    </button>
 )
+
+Button.propTypes = {
+    handleClick: React.PropTypes.func
+}
 
 export default Button
---
+++ src/components/Button/likeBtnFb.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+import Button from './buttonIndex'
+
+
+const LikeButton = () => (
+    <Button> Curtir</Button>
+)
+
+export default LikeButton
