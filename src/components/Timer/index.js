ff3401bf01f759f6076c97071cfca3ef44c255c5
Nayara - Home <ncy.fers@gmail.com>
2019-09-29 21:09:59 -0300

    2019-09-29-#13: criacao do componente timer

---
+++ src/components/Timer/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Timer extends Component {
+    constructor () {
+    super()
+    this.state = {
+        time: 0
+        }
+    this.timer;
+    }
+    componentDidMount () {
+        this.timer = setInterval(() => {
+            this.setState({ 
+                time: this.state.time + 1 })
+        }, 1000)
+    }
+
+    componentWillMount () {
+        clearInterval:{this.time}
+    }
+    render () {
+        return <div>Timer: {this.state.time}</div>
+    }
+}
+
+export default Timer
